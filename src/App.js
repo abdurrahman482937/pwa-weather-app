b552e82a5ac6487479ceeb2b157a8db31a1522c3
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-23 17:34:22 +0600

    3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
-import fetchWeather from './api/fetchWeather';
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 export default function App() {
     let [query, setQuery] = useState("")
+    let [weather, setWeather] = useState("")
+    const search = async (e) => {
+        if (e.key === "Enter") {
+            const data = await fetchWeather(query)
+            setWeather(data);
+            setQuery("");
+        }
+    }
     return (
         <div className='main-container'>
             <input
@@ -14,6 +22,7 @@ export default function App() {
                 onChange={(e) => {
                     setQuery(e.target.value)
                 }}
+                onKeyDown={search}
             />
         </div>
     );
