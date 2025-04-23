97852671b3af34a00ac50d7e7864d044e32bbcbd
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-23 17:34:27 +0600

    5

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import axios from "axios";
+import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
-export default async function fetchWeather(query) {
+export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
         params: {
             q: query,
-            units: "metric",
-            APPID: API_KEY
+            units: 'metric',
+            APPID: API_KEY,
         }
-    })
+    });
+    return data;
 }
