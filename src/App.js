76e97720365577c56efa6221b89b720d6c6ba5fb
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-23 17:35:15 +0600

    8

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ export default function App() {
     }
     return (
         <div className='main-container'>
-            <input type='text' className='search'
-                placeholder='Search...'
+            <input type='text' className='search' placeholder='Search...'
                 value={query}
                 onChange={(e) => {
                     setQuery(e.target.value)
